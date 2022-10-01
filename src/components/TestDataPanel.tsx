c8179f0f57e7e5543bfbcec75e9e71a43fd19fd5
Mikhail Bakalov <mbakalov@microsoft.com>
2022-10-01 07:16:47 -0700

    Adding upload panel layout - monaco editor
    Switch to react-app-rewired for monaco editor support.

--- src/components/TestDataPanel.tsx
+++ src/components/TestDataPanel.tsx
@@ -1,5 +1,37 @@
+import {
+  Checkbox,
+  DefaultButton,
+  Stack,
+  mergeStyles
+} from '@fluentui/react';
+
+import MonacoEditor from 'react-monaco-editor';
+
 export const TestDataPanel = () => {
+
+  const editorStyles = mergeStyles({
+    float: 'left',
+    width: '50%'
+  });
+  
+  const resultsStyles = editorStyles;
+
   return (
-    <div>TEST DATA PANEL</div>
+    <Stack tokens={{childrenGap: 10, padding: 10}}>
+      <Stack horizontal tokens={{childrenGap: 10}}>
+        <DefaultButton text='Upload' />
+        <Stack.Item align='center'>
+          <Checkbox label='Add current user to all threads' disabled defaultChecked />
+        </Stack.Item>
+      </Stack>
+      <div>
+        <div className={editorStyles}>
+          <MonacoEditor
+            language='json'
+          />
+        </div>
+        <div className={resultsStyles}>RESULTS</div>
+      </div>
+    </Stack>
   );
 }
